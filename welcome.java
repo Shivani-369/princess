15f5a096283a746ce9170ac60c452b237cd9f243
shivani <shivani63124@gmail.com>
2025-07-11 19:35:48 +0530

    added the java file to welcome the user

---
+++ welcome.java
@@ -0,0 +1,10 @@
+package princess;
+
+public class welcome {
+    public static void main(String[] args){
+    System.out.println("-*-".repeat(10));
+    System.out.println("   Welcome to java");
+    System.out.println("-*-".repeat(10));
+    }
+    
+}
